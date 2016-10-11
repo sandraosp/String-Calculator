c89c22755b00840400b3f180f517ae14d0767822
Sandra Ösp Stefánsdóttir <sandraospstefansdottir@Sandras-MacBook-Pro.local>
2016-10-11 14:31:50 +0000

    Extra testcases for step 1 in Kata Steps

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -21,19 +21,25 @@ public class CalculatorTest
 	public void testOneNumber() 
 	{
 		assertEquals(1, Calculator.add("1"));
+		assertEquals(2, Calculator.add("2"));
+		assertEquals(3, Calculator.add("3"));
+		assertEquals(4, Calculator.add("4"));
 	}
 
 	@Test
 	public void testTwoNumbers() 
 	{
 		assertEquals(3, Calculator.add("1,2"));
+		assertEquals(4, Calculator.add("2,2"));
+		assertEquals(5, Calculator.add("2,3"));
 	}	
 
 	@Test
     public void testMultipleNumbers()
     {
     	assertEquals(6, Calculator.add("1,2,3"));
+    	assertEquals(6, Calculator.add("2,2,2"));
     }
-    
+
 
 }
